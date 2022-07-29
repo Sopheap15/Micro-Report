ec0ba7b118d2e563809c8cf3072817f5ca3ed133
Sopheap <sopheap15@gmail.com>
2022-07-29 16:59:45 +0700

    Modify Run.R

--- Run.R
+++ Run.R
@@ -5,12 +5,7 @@ source("code/Load_data.R",knitr::knit_global())
 
 # Render report
 rmarkdown::render("code/Microbiology Report.Rmd",
-                    output_file = str_glue("Outputs/{dic$short_name}_Microbiology_Report_{Sys.Date()}.html"), quiet = T)
-
-
-
-
-
+                    output_file = str_glue("outputs/{dic$short_name}_Microbiology_Report_{Sys.Date()}.html"), quiet = T)
 
 
 
