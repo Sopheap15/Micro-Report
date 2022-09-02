b4294293be401f5b5be90b12e062526088744651
Sopheap <sopheap15@gmail.com>
2022-09-02 10:01:10 +0700

    file name with underscore

--- Run.R
+++ Run.R
@@ -1,9 +1,10 @@
 # Load data (Shortcut CTR|Command + A + Enter)----
 rm(list = ls())
+Sys.setlocale(category = "LC_ALL", locale = "en_US.UTF-8")
 source("code/Load_data.R",knitr::knit_global())
 
 # Render report
-rmarkdown::render("code/Microbiology Report.Rmd",
+rmarkdown::render("code/Microbiology_Report.Rmd",
                     output_file = str_glue("outputs/{dic$short_name}_Microbiology_Report_{Sys.Date()}.html"), quiet = T)
 
 
