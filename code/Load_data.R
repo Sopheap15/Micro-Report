a7b3edd5c06cb324a546a063dca66084d76efb74
Sopheap <sopheap15@gmail.com>
2022-09-12 15:52:58 +0700

    Update reg for TAT

--- code/Load_data.R
+++ code/Load_data.R
@@ -13,7 +13,7 @@ month <- data.frame(collection_date_in_month = factor(
   levels = month.abb
 ))
 
-# list of organism in notifiable----
+# Notifiable organisms in MoH ----
 org_in_hos <- c(
   "Klebsiella pneumoniae",
   "Acinetobacter",
@@ -37,12 +37,16 @@ cont_org_list <- c(
 
 # Load dictionary ----
 dic <-
-  list.files(path = "data",
-             pattern = "^[Dd]ic.*(\\s)?.xls(x)?",
-             full.names = T) %>%
-  read_excel(sheet = "hospital",
-             col_names = T,
-             trim_ws = T)
+  list.files(
+    path = "data",
+    pattern = "^[Dd]ic.*(\\s)?.xls(x)?",
+    full.names = T
+  ) %>%
+  read_excel(
+    sheet = "hospital",
+    col_names = T,
+    trim_ws = T
+  )
 
 dic <- dic %>%
   select(parameter = Parameter, full_name = `Full name`) %>%
@@ -57,63 +61,62 @@ dic <- dic %>%
       hospital_name == "All laboratories" ~ "All"
     ),
     hospital_name = recode(hospital_name,
-                           "All laboratories" = "Siem Reap, Takeo, Battambang and Kampong Cham Provincial Referral Hospital")
+      "All laboratories" = "Siem Reap, 
+      Takeo, Battambang and Kampong Cham Provincial Referral Hospital"
+    )
   ) %>%
   mutate_at(vars(contains("date")), as.Date, format = "%d-%m-%Y")
 
 # Load data ----
-# Read all file in folder data
 read_file <- function(path) {
   path %>%
     read_excel(na = c("", "NA"), trim_ws = T) %>%
     clean_names() %>%
-    select(-any_of(
-      c(
-        "no",
-        "dob",
-        "test_date",
-        "ampi_peni",
-        "cephalosporins",
-        "fluoroquinolone",
-        "gentamicin_synergy",
-        "novobiocin",
-        "metronidazole",
-        "oral_cephalosporins"
-      )
-    ))
+    select(-any_of(c(
+      "no", "dob", "test_date", "ampi_peni", "cephalosporins", "fluoroquinolone",
+      "gentamicin_synergy", "novobiocin", "metronidazole", "oral_cephalosporins"
+    )))
 }
 
 # format date
 format_date <- function(d) {
   if (is.character(d$collection_date) == TRUE) {
     d %>%
-      mutate_at(vars(contains("date")),
-                ~ as.POSIXct(.,
-                             format = "%d-%B-%Y %H:%M",
-                             tz = "GMT"))
+      mutate_at(
+        vars(contains("date")),
+        ~ as.POSIXct(.,
+          format = "%d-%B-%Y %H:%M",
+          tz = "GMT"
+        )
+      )
   } else if (is.numeric(d$collection_date) == TRUE) {
     d %>%
-      mutate_at(vars(contains("date")),
-                ~ as.POSIXct(. * (60 * 60 * 24),
-                             origin = "1899-12-30",
-                             tz = "GMT"))
+      mutate_at(
+        vars(contains("date")),
+        ~ as.POSIXct(. * (60 * 60 * 24),
+          origin = "1899-12-30",
+          tz = "GMT"
+        )
+      )
   } else {
     "Datetime is in correct format"
   }
 }
 
-data <- list.files(path = "data",
-                   pattern = "[Bb]ac.*port.*.xls(x)?",
-                   full.names = T) %>%
+data <- list.files(
+  path = "data",
+  pattern = "[Bb]ac.*port.*.xls(x)?",
+  full.names = T
+) %>%
   purrr::discard(file_name, .p = ~ stringr::str_detect(., "~")) %>%
   map_df(read_file) %>%
   mutate(sex = factor(sex)) %>%
   format_date()
 
-data <- data %>% # Filter lab name
+data <- data %>% # Filter based on lab name
   filter(lab_name == ifelse(dic$short_name == "All", lab_name, dic$short_name))
 
-data <- data %>% # Filter date
+data <- data %>% # Filter based on date
   arrange(collection_date) %>%
   mutate(col_date = as.Date(collection_date)) %>%
   filter(
@@ -130,7 +133,8 @@ data <-
   data %>% # Recode specimen and mutate column collection_date_in month
   mutate(
     collection_date_in_month = factor(format(collection_date, "%b"),
-                                      levels = month.abb),
+      levels = month.abb
+    ),
     sample = recode(
       sample,
       "Pus aspirate" = "Pus",
@@ -142,11 +146,12 @@ data <-
 
 data <- data %>% # Deduplicate data
   distinct(patient_id,
-           lab_id,
-           collection_date,
-           sample,
-           results,
-           .keep_all = T)
+    lab_id,
+    collection_date,
+    sample,
+    results,
+    .keep_all = T
+  )
 
 # write_rds(data, compress = "none", "Outputs/clean_data")
 
@@ -154,19 +159,22 @@ data <- data %>% # Deduplicate data
 
 
 reject_spe <- data %>% # reject specimen
-  select(patient_id,
-         lab_id,
-         collection_date,
-         sample,
-         results,
-         comment,
-         reject_comment) %>%
+  select(
+    patient_id,
+    lab_id,
+    collection_date,
+    sample,
+    results,
+    comment,
+    reject_comment
+  ) %>%
   distinct(patient_id,
-           lab_id,
-           collection_date,
-           sample,
-           results,
-           .keep_all = T)
+    lab_id,
+    collection_date,
+    sample,
+    results,
+    .keep_all = T
+  )
 
 
 # Age group
@@ -205,7 +213,7 @@ data <- data %>%
 # Remove unusual string and convert to standard name
 data <- data %>%
   mutate(
-    results = str_remove(results, "(\\s)?sp(p|.)?$"),
+    results = str_remove(results, "(\\s)?sp.*$"),
     # remove sp. or spp.
     results = str_remove(results, "(\\s)?\\d$"),
     # remove number
@@ -217,14 +225,14 @@ data <- data %>%
     # remove word rods
     results = str_remove(results, "non-[tT]yphi$|non-[tT]yphi/non-[pP]aratyphi$"),
     results = str_remove(results, ",(\\s)?$"),
-    results = trimws(results, "both")
+    results = str_trim(results, "both")
     # mo = as.mo(results, info = F) # convert to standard name
   )
 
 # Convert antibiotic name to standard
 data <- data %>%
   rename_with(as.ab, amikacin:vancomycin) %>%
-  mutate(across(where(is.rsi.eligible), as.rsi))
+  mutate_if(is.rsi.eligible, as.rsi)
 
 
 # Finding last month
@@ -233,12 +241,16 @@ last_month <- data %>%
 
 # Read and Joint ward data ----
 ward <-
-  list.files(path = "data",
-             pattern = "^[Dd]ic.*.xls(x)?",
-             full.names = T) %>%
-  read_excel(sheet = "ward",
-             col_names = T,
-             trim_ws = T)
+  list.files(
+    path = "data",
+    pattern = "^[Dd]ic.*.xls(x)?",
+    full.names = T
+  ) %>%
+  read_excel(
+    sheet = "ward",
+    col_names = T,
+    trim_ws = T
+  )
 
 data <-
   left_join(data, ward, by = c("sample_source" = "ward_from_Camlis")) %>%
@@ -262,8 +274,10 @@ dedup_by_id_stype <- data %>%
 
 # Blood culture first isolate----
 bc_first_isolate <- data %>%
-  filter(sample == "Blood Culture",
-         !results %in% c(cont_org_list, "No growth")) %>%
+  filter(
+    sample == "Blood Culture",
+    !results %in% c(cont_org_list, "No growth")
+  ) %>%
   mutate(mo = as.mo(results, info = F)) %>%
   filter_first_isolate(
     episode_day = 30,
@@ -304,27 +318,27 @@ comment <- data %>%
   select(lab_name, sample, results, comment) %>%
   filter(sample == "Blood Culture", !is.na(comment)) %>%
   mutate(
-    comment = str_replace_all(
-      str_to_lower(comment),
-      c(
-        "\\bone" = "1",
-        "\\btwo" = "2",
-        "\\bthree" = "3",
-        "\\bfour" = "4",
-        "\\bfive" = "5",
-        "\\bsix" = "6",
-        "\\bseven" = "7",
-        "\\beight" = "8",
-        "\\bnine" = "9",
-        "\\bzero" = "0"
-      )
-    ),
-    bottle_pos = str_extract(comment,
-                             pattern = "\\d\\s?of\\s?\\d"),
-    day_growth = str_extract(comment,
-                             pattern = "day.*?\\d|\\d.?day.?"),
-    day_growth = str_extract(day_growth,
-                             pattern = "\\d")
+    comment = str_replace_all(str_to_lower(comment), 
+                              c("\\bone" = "1",
+                                "\\btwo" = "2",
+                                "\\bthree" = "3",
+                                "\\bfour" = "4",
+                                "\\bfive" = "5",
+                                "\\bsix" = "6",
+                                "\\bseven" = "7",
+                                "\\beight" = "8",
+                                "\\bnine" = "9",
+                                "\\bzero" = "0"
+                              )),
+    bottle_pos = str_extract(comment, pattern = "\\d\\s?[oO]f\\s?\\d"),
+    day_growth = str_extract(comment, "\\d(?=\\s?day)|(?<=[ys]\\s?)\\d")
+    
+    # day_growth = str_extract(comment,
+    #   pattern = "day.*?\\d|\\d.?day.?"
+    # ),
+    # day_growth = str_extract(day_growth,
+    #   pattern = "\\d"
+    # )
   )
 
 # Critical results detection
@@ -333,9 +347,8 @@ critical_result <- data %>%
   distinct(patient_id, results, .keep_all = T) %>%
   select(comment) %>%
   mutate(
-    call = str_detect(
-      str_to_lower(comment),
-      "call[ed]?|phon[ed]?|dr(\\.+)?|clinician[s]?"
+    call = str_detect(str_to_lower(comment),
+                      "call[ed]?|phon[ed]?|dr(\\.+)?|clinician[s]?"
     ),
     total = n()
   ) %>%
@@ -346,13 +359,14 @@ critical_result <- data %>%
 
 # TAT
 TAT <- data %>%
-  select(lab_name,
+  select(lab_name, 
          patient_id,
          collection_date,
          admission_date,
          sample,
          results,
-         comment) %>%
+         comment
+  ) %>%
   filter(
     !is.na(results),
     sample == "Blood Culture",
@@ -362,12 +376,13 @@ TAT <- data %>%
   distinct(patient_id, results, .keep_all = T) %>%
   mutate(
     comment_by = str_extract(str_to_lower(comment),
-                             pattern = "(\\.*)?(call[ed]?|phon[ed]?)(.*)"),
+      pattern = "(\\.*)?(call[ed]?|phon[ed]?)(.*)"
+    ),
     comment_by = str_replace(comment_by,
-                             pattern = "(die[d]|dead) (on)? \\d+[/ ,.-]\\d+[/ ,.-]\\d+", ""),
+      pattern = "(die[d]|dead) (on)? \\d+[/ ,.-]\\d+[/ ,.-]\\d+", ""
+    ),
     date = str_extract(comment_by, pattern = "\\d+[/ ,.-]\\d+[/ ,.-]\\d+"),
-    
-    time = str_extract(comment_by, pattern = "\\d+[ : .;](\\s)?\\d+(\\s)?([AaPp][Mm])?")
+    time = str_extract(comment_by, pattern = "\\d+(\\s)?[ :.;](\\s)?\\d+(\\s)?([AaPp][Mm])?")
   ) %>%
   filter(!is.na(comment_by)) %>%
   mutate(primary_report = lubridate::dmy_hm(paste(date, time), quiet = TRUE))
